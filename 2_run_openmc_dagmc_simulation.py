bea408ef9444d8bae8eceb7eadd0eeaea190dcaa
Jonathan Shimwell <mail@jshimwell.com>
2022-03-24 14:59:31 +0000

    added details on angles

--- 2_run_openmc_dagmc_simulation.py
+++ 2_run_openmc_dagmc_simulation.py
@@ -150,8 +150,9 @@ my_source = openmc.Source()
 radius = openmc.stats.Discrete([293.], [1])
 # the distribution of source z values is just a single value
 z_values = openmc.stats.Discrete([0], [1])
-# the distribution of source azimuthal angles values is a uniform distribution between 0 and 2 Pi
-angle = openmc.stats.Uniform(a=0., b=0.5* 3.14159265359)
+# the distribution of source azimuthal angles values is a uniform distribution between 0 and 0.5 Pi
+# these angles must be the same as the reflective angles
+angle = openmc.stats.Uniform(a=0., b=math.radians(90))
 # this makes the ring source using the three distributions and a radius
 my_source.space = openmc.stats.CylindricalIndependent(r=radius, phi=angle, z=z_values, origin=(0.0, 0.0, 0.0))
 # sets the direction to isotropic
